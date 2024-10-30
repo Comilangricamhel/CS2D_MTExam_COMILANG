e3587d20fe34b07c084e52a29611e31a6a60a9f9
Comilangricamhel <carolcomilang06@gmail.com>
2024-10-30 17:46:51 +0800

    Create classSubjects.js

---
+++ classSubjects.js
@@ -0,0 +1,8 @@
+let studentName = "Karla";              
+let subjectTitle = "Science";               
+let classSchedule = "Monday and Wednesday, 10:00 AM - 11:30 AM"; 
+let classroom = "Room 103";                     
+let classInstructor = "Ms. Rosales";              
+
+// Log the information to the console
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.`);
