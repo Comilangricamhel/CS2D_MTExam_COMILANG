3303eee9826ad58ee7ae913d845fa9355f26d76a
Comilangricamhel <carolcomilang06@gmail.com>
2024-10-31 07:08:34 +0800

    Create Problem6.js

---
+++ Problem6.js
@@ -0,0 +1,9 @@
+let userProfile = {
+    fullName:prompt(`Enter your name:  `),  
+    age:prompt (`Enter your age: `),                
+    favoriteNumber:prompt (`Enter your Favorite Number:  `)      
+    favoriteColors:prompt (`Enter your Favorite Color:  `) 
+};
+
+// Log the entire object to the console
+console.log(userProfile);
