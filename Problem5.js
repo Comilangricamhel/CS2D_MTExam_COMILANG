5bf76455d62dcbb723eb3d1c78d3f3bd935e42cd
Comilangricamhel <carolcomilang06@gmail.com>
2024-10-31 08:07:05 +0800

    Create Problem5.js

---
+++ Problem5.js
@@ -0,0 +1,11 @@
+let favoriteColors = []; // empty array
+
+// the  following just prompt user to add three colors
+  let color = prompt("First a color you like:");
+  console.log(favoritecolor);
+  
+  favoriteColors.push("Second a color you like");
+  console.log(favoritecolor)
+  // Log the updated array to the console
+  console.log("Updated color list:", favoriteColors);
+}
